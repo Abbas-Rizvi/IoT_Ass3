45fc0a1f6f5e0379fcf5f731d05a78a22161c1ea
Abbas Rizvi <abbas8rizvi@gmail.com>
2023-11-16 04:46:33 -0500

    removed venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/abbas/documents/uni/yr4/iot/assign/ass3/venv"
+set -gx VIRTUAL_ENV "/home/abbas/documents/uni/yr4/iot/ass/IoT_Ass3/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
